51d978494eef29d08f580529e7a31261ae5f53d4
David Rauschenbach <davidr@imap.cc>
2017-08-14 20:57:43 -0700

    Read more than one line before yielding, when time permits

--- SocketInputDStream.lua
+++ SocketInputDStream.lua
@@ -15,16 +15,24 @@ function SocketInputDStream:compute(durationBudget)
   
   -- Connect
   local conn = socket.connect(self.hostname, self.port)
-  conn:settimeout(durationBudget)
   
-  -- run loop
+  -- run loop. Read multiple lines until the duration budget has elapsed
+  local timeOfLastYield = socket.gettime()
+  local rdds = {}
+  local minWait = 0.02 -- never block less than a 20ms "average context switch"
   while true do
-    local line, err = conn:receive('*l')
-    local rdds
-    if not err then
-      rdds = {self.sc:makeRDD({line})}
+    local elapsed = socket.gettime() - timeOfLastYield
+    if elapsed > durationBudget then
+      coroutine.yield(rdds)
+      rdds = {}
+      timeOfLastYield = socket.gettime()
+    else
+      conn:settimeout(math.max(minWait, durationBudget - elapsed))
+      local line, err = conn:receive('*l')
+      if not err then
+        rdds[#rdds+1] = self.sc:makeRDD({line})
+      end
     end
-    coroutine.yield(rdds)
   end
   
 end
