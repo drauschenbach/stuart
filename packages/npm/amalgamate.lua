c56de322dbbcbd48e0c9a4b366f52554fb5a02de
David Rauschenbach <davidr@imap.cc>
2018-09-21 13:18:38 -0700

    Generated amalg cache now includes transitive dependencies

--- packages/npm/amalgamate.lua
+++ packages/npm/amalgamate.lua
@@ -1,11 +1,31 @@
-local rockspecFile = arg[1]
-if rockspecFile == nil then
+-- Helper function
+local function getModulesForDependency(dependency)
+  local cmd = 'luarocks show ' .. dependency .. ' --modules'
+  local f = assert(io.popen(cmd, 'r'))
+  local s = assert(f:read('*a'))
+  f:close()
+  s = string.gsub(s, '^%s+', '')
+  s = string.gsub(s, '%s+$', '')
+  s = string.gsub(s, '[\n\r]+', ' ')
+  local t = {}
+  for k in string.gmatch(s, '%S+') do
+    if k ~= 'moses_min' then
+      t[#t+1] = k
+    end
+  end
+  return t
+end
+
+
+-- main
+local rockspecFilespec = arg[1]
+if rockspecFilespec == nil then
   error('A rockspec argument is required')
 end
 
 
 -- Run the rockspec file, which sets certain global variables
-local rockspecFn = assert(loadfile(rockspecFile))
+local rockspecFn = assert(loadfile(rockspecFilespec))
 rockspecFn()
 
 
@@ -21,7 +41,10 @@ for _,v in ipairs(dependencies) do
   local spaceAt = string.find(v, ' ')
   local dependency = v:sub(1, spaceAt-1)
   if dependency ~= 'lua' then
-    -- TODO
+    local modules = getModulesForDependency(dependency)
+    for _, module in ipairs(modules) do
+      amalgCache[module] = 'L'
+    end
   end 
 end
 
@@ -30,11 +53,15 @@ end
 local file = io.open('amalg.cache', 'w')
 file:write('return {\n')
 for k,v in pairs(amalgCache) do
-  file:write('  ["' .. k .. '"] = "L",\n')
+  file:write('  ["' .. k .. '"] = "' .. v .. '",\n')
 end
 file:write('}\n')
 file:close()
 
 
--- Invoke amalg.lua to produce a single Lua file
+-- Invoke amalg.lua to produce a single stuart.lua file
 os.execute('amalg.lua -s empty.lua -o stuart.lua -c')
+
+
+-- Produce a stuart-min.lua file
+-- TODO
